4f73251520398b12456751e21a565b2bd1148e54
Greg Chapman <gregchapman@Gregs-MacBook-Pro-2.local>
2026-01-20 23:19:16 -0500

    Update sitemap to remove deleted routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,18 +10,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: `${baseUrl}/about`,
-            lastModified: new Date(),
-            changeFrequency: "monthly",
-            priority: 0.8,
-        },
-        {
-            url: `${baseUrl}/notes`,
-            lastModified: new Date(),
-            changeFrequency: "weekly",
-            priority: 0.8,
-        },
-        {
             url: `${baseUrl}/gallery`,
             lastModified: new Date(),
             changeFrequency: "monthly",
